9439161941b9762d0622c0c0dac33e5e0c50ab60
kase15 <zeewe.3@gmail.com>
2024-04-09 11:40:43 +0900

    prisma導入等
    .envが本当にignoreされているのが不明、上げてみて要確認

---
+++ prisma/migrations/20240409015736_create_company_and_user/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" TEXT,
+    "companyId" INTEGER NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
